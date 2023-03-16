790feffefd83925f1436af9eeb2bdd2b27b61e85
eilmadc@gmail.com <eilmadc@gmail.com>
2023-03-16 07:50:19 +0100

    Update Ejercicio1.java

--- src/clases/Ejercicio1.java
+++ src/clases/Ejercicio1.java
@@ -1,14 +1,24 @@
 /**
- * 
+ * C2-UD07- Ejercicio1
  */
 package clases;
 
 /**
  * @author elena-01
- *
+ * Calcular nota media alumnos
  */
 public class Ejercicio1 {
 
 	
+	calculaNotaMedia
+	s
+	
+	//Crear array alumnos-notas
+	
+	//Rellenar array
+	
+	//Calcular Nota media
+	
+	//Almacenar en Array de datos alumno-notamedia
 	
 }
